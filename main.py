00a9ac28c35bf928e7f1d39dea7b499d555ab646
Guy Ness <guyness03@gmail.com>
2025-12-29 14:00:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
+# Download the data from your GitHub repository
+!wget https://raw.githubusercontent.com/yotam-biu/ps9/main/parkinsons.csv -O /content/parkinsons.csv
+!wget https://raw.githubusercontent.com/yotam-biu/python_utils/main/lab_setup_do_not_edit.py -O /content/lab_setup_do_not_edit.py
+import lab_setup_do_not_edit
 
+import pandas as pd
+
+df = pd.read_csv('parkinsons.csv')
+df = df.dropna()
+
+selected_features = ['MDVP:Fo(Hz)', 'MDVP:Jitter(%)']
+X = df[selected_features]
+y = df['status']
+
+from sklearn.preprocessing import MinMaxScaler
+
+scaler = MinMaxScaler()
+X = scaler.fit_transform(X)
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+from sklearn.tree import DecisionTreeClassifier
+
+model = DecisionTreeClassifier(max_depth = 20, random_state = 42)
+model.fit(X_train, y_train)
+
+from sklearn.metrics import accuracy_score
+
+y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+print(f'Accuracy: {accuracy}')
+
+import joblib
+
+joblib.dump(model, 'my_model.joblib')
